be2b2399723b3f7396644a48c6fd82c585d6ed83
Gurman61 <78517005+Gurman61@users.noreply.github.com>
2021-11-03 20:04:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{}
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.position(100,300);
+    
+}
+function draw()
+{}
+
+
+
+function take_snapshot()
+{
+    save("student_name.png");
+}
